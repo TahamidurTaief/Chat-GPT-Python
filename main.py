56b608d49eae9e432713fca5a34913f58754ce1a
Tahamidur Taief <99859434+TahamidurTaief@users.noreply.github.com>
2023-01-13 17:36:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import openai
+
+openai.api_key = "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"
+
+while True:
+	ask = input('\nHUMAN: \n')
+	response = openai.Completion.create(model="text-davinci-003",
+	                                    prompt=ask,
+	                                    temperature=0.9,
+	                                    max_tokens=150,
+	                                    top_p=1,
+	                                    frequency_penalty=0,
+	                                    presence_penalty=0.6,
+	                                    stop=[" Human:", " AI:"])
+
+	text = response['choices'][0]['text']
+	print('\nCHAT GPT: ' + text)
+
+# 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
